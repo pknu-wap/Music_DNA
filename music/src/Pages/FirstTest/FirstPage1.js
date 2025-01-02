931a9d7c6ea8c913bea7ea3b8366b47224d327f4
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2025-01-02 15:28:59 +0900

    fix: 장르 변경
    rock1개, pop2개, hip2개에서 rock1개, jazz2개, rb2개로 변경

--- music/src/Pages/FirstTest/FirstPage1.js
+++ music/src/Pages/FirstTest/FirstPage1.js
@@ -86,9 +86,9 @@ const FirstPage1 = () => {
           <div className="nextButton">
             <NextButton
               to="/SecondPage"
-              popCount={2}
-              hipCount={2}
               rockCount={1}
+              jazzCount={2}
+              RbCount={2}
             />
           </div>
         </div>
